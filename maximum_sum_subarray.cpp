4fabf3977ad51c3b8f1366bf01102b5099ffc04c
Kaustubhhub <90435586+Kaustubhhub@users.noreply.github.com>
2023-06-05 22:27:35 +0530

    Add files via upload

---
+++ maximum_sum_subarray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxi = LONG_MIN;
+    long long sum = 0;
+
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+
+        maxi = max(maxi, sum);
+
+        if(sum < 0)sum = 0;
+    }
+    if(maxi < 0)return 0;
+    return maxi;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    vector<int>ans = maxSubarraySum(arr,n);
+}
